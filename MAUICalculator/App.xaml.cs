1d00c0ed70d27738adfc41ab0f5c93313361eadd
Your Name <you@example.com>
2024-02-23 16:14:39 +0000

    first commit

---
+++ MAUICalculator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MAUICalculator.MVVM;
+
+namespace MAUICalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CalcView();
+        }
+    }
+}
